3359ddb4615833800c4dc0ec1f247c6f3e43193e
Makayla Kuhnke <112013026+makalaaka@users.noreply.github.com>
2022-10-19 12:57:16 -0700

    pastel life

--- src/LEDClient.java
+++ src/LEDClient.java
@@ -5,7 +5,7 @@ import org.zeromq.ZMQ;
 import org.zeromq.ZContext;
 import java.util.concurrent.TimeUnit;
 
-public class LEDClient {
+class LEDClient {
     private ZContext zctx;
     private ZMQ.Socket zsocket;
     private Gson gson;
@@ -29,8 +29,8 @@ public class LEDClient {
         zsocket.send(message);
     }
 
-    public void blinkN(int[] color, int times, int miliseconds) throws  InterruptedException{
-        for(int i=0; i<times; i++) {
+    public void strobe(int[] color, int times, int miliseconds) throws InterruptedException {
+        for (int i = 0; i < times; i++) {
             send(color);
             TimeUnit.MILLISECONDS.sleep(miliseconds);
             send(LEDClient.OFF);
@@ -44,14 +44,17 @@ public class LEDClient {
         this.zctx.close();
     }
 
-    public static void main(String[] args) {
-        LEDClient ledClient = new LEDClient("tcp", "192.168.86.250", 5001);
-        try {
-            int[] color = {0, 0, 255};
-            ledClient.blinkN(color, 5, 1000);
-            ledClient.close();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+    public static void main(String[] args) throws InterruptedException {
+        LEDClient ledClient = new LEDClient("tcp", "192.168.1.117", 5001);
+
+        int[] color1 = {224, 64, 251}; // purple
+        int[] color2 = {64, 196, 255}; //blue
+        int[] color3 = {255, 111, 0}; //orange
+        int[][] colors = {color1, color2, color3};
+
+        for (int i = 0; i < 3; i++) {
+            ledClient.strobe(colors[i], 5, 500);
         }
+
     }
 }
